2271a92f3b34c323e231d80936c14f38a94827a7
venkat kunapuli <venkat.kunapuli@rockdovesolutions.com>
2015-06-23 16:39:45 +0000

    updates seacrh query parsing

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var qs = require('qs');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,7 +7,12 @@ router.get('/', function(req, res) {
   res.render('index.html')
 });
 router.post('/foodQuery', function(req, res) {
-    res.render('query_reports.html', {query_url: req.body.query});
+    var qObj = qs.parse(req.body.query);
+    var qString = '';
+    for (var key in qObj)
+      qString += key + ':'+ qObj[key] + '+';
+    console.log(qString);
+    res.render('query_reports.html', {query_url: qString});
 });
 
 module.exports = router;
