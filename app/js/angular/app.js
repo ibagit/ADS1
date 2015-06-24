705260838bbc3c9d59183530e5940a58f1a278ec
Devon-Peroutky <devonperoutky@vividcortex.com>
2015-06-24 10:50:42 -0400

    Search on Distribution Pattern from State, moved Map logic into controller

--- app/js/angular/app.js
+++ app/js/angular/app.js
@@ -8,7 +8,7 @@ rdApp.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl : 'pages/default.htm',
-            controller  : 'tempCtrl'
+            controller  : 'mapCtrl'
         })
         .when('/contact', {
             templateUrl : 'pages/contact.htm',
@@ -50,16 +50,70 @@ rdApp.service('storage', function() {
 });
 
 /* Controllers */
+// Temp Controller 
 rdApp.controller('tempCtrl', ['$scope', function($scope) {
-    console.log("Controller!");
+    console.log("Temp Controller!");
 }]);
 
+// Map Controller
+rdApp.controller('mapCtrl', ['$scope', function($scope) {
+    console.log("Map Controller!");
+
+    // Get User's Latitude & Longitude
+    function success(position) {
+
+        // Get the Latitude & Longitude
+        var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        // Logging
+        console.log("The user's location is: " + latlng.toString());
+
+        // Convert the LatLong to String
+        codeLatLng(latlng);
+    }
+
+    // Error Handler
+    function error(msg) {
+        var s = document.querySelector('#status');
+        console.log("Could not gain access to User Location");
+    }
+
+    // Initiate process of receiving user location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        error('not supported');
+    }
+
+    // Covert Lat & Long coordinates into State            
+    function codeLatLng(coordinate) {
+        var geocoder = new google.maps.Geocoder();
+        console.log("Successfully initialized Geocoder...");
+        geocoder.geocode({'latLng': coordinate}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                    console.log("Full Address: " + results[0].formatted_address);
+                    console.log("State: " + results[5].formatted_address.split(",")[0]);
+                    //window.location = '/#/form/'+region + '/' + code;
+                } else {
+                    alert('No results found');
+                }
+            } else {
+                alert('Geocoder failed due to: ' + status);
+            }
+        });
+    }
+}]);
+
+
+// Results Controller
 rdApp.controller('resultsCtrl', ['$scope', 'storage', function($scope, storage) {
     console.log("Results Controller!");
     $scope.recalls = storage.getData();
     console.log($scope.recalls);
 }]);
 
+// Form Controller
 rdApp.controller('formCtrl', ['$scope', '$routeParams', '$http', 'storage', function($scope, $routeParams, $http, storage) {
     $scope.recalls = "";                
     $scope.state = $routeParams.state;
@@ -82,7 +136,7 @@ rdApp.controller('formCtrl', ['$scope', '$routeParams', '$http', 'storage', func
 
         $http.post('/foodQuery', { 
             params: parms,
-            'state': $scope.stateCode
+            'distribution_pattern': $scope.stateCode
         })
         .success(function(results) {
             console.log("Success?!?!?");
