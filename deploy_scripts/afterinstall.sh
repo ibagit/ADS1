8bc956109cdf65f5b2d5f90876a42efc074f094a
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-25 15:58:22 -0400

    Rev. 15

--- deploy_scripts/afterinstall.sh
+++ deploy_scripts/afterinstall.sh
@@ -3,11 +3,11 @@ pushd .
 cd /var/ibagit/ADS1
 #need. the pushd and popd because codedeploy with cd
 #will break the wholeagent
-apt-get update
-apt-get -y install nodejs 
-apt-get -y install node npm
-apt-get -y install nodejs-legacy
-#npm install
+#apt-get update
+#apt-get -y install nodejs 
+#apt-get -y install node npm
+#apt-get -y install nodejs-legacy
+npm install
 #npm install express
 #npm install bower
 #npm start &
