fa2efbc2ad1df72b5c0f8fbb63a82d2b55b83c7a
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-25 17:01:15 -0400

    Rev.

--- deploy_scripts/afterinstall.sh
+++ deploy_scripts/afterinstall.sh
@@ -4,6 +4,6 @@ cd /var/ibagit/ADS1
 #need. the pushd and popd because codedeploy with cd
 #will break the wholeagent
 apt-get update
-sudo pt-get -y install nodejs --allow-root
+apt-get -y install nodejs
 sudo apt-get npm install
 popd
