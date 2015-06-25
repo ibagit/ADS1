c6d402e5dab8be13f39671643c23503c006b14ec
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-25 17:45:55 -0400

    Rev

--- deploy_scripts/afterinstall.sh
+++ deploy_scripts/afterinstall.sh
@@ -5,5 +5,14 @@ cd /var/ibagit/ADS1/
 #will break the wholeagent
 apt-get update
 apt-get -y install nodejs npm
-apt-get -y install bower
+npm install bower
+npm install body-parser
+npm install cookie-parser
+npm install debug
+npm install express
+npm install jade
+npm install morgan
+npm install serve-favicon
+npm install request
+npm install ejs
 popd
