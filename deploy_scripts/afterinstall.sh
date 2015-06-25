b3229954fcb8df1bd101ec82006655ddbb53734c
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-25 18:53:46 -0400

    Rev

--- deploy_scripts/afterinstall.sh
+++ deploy_scripts/afterinstall.sh
@@ -16,12 +16,12 @@ sudo npm install morgan
 sudo npm install serve-favicon
 sudo npm install request
 sudo npm install ejs
-sudo bower install angular
-sudo bower install angular-mocks
-sudo bower install jquery
-sudo bower install bootstrap
-sudo bower install angular-route
-sudo bower install angular-resource
-sudo bower install angular-animate
+sudo npm install angular
+sudo npm install angular-mocks
+sudo npm install jquery
+sudo npm install bootstrap
+sudo npm install angular-route
+sudo npm install angular-resource
+sudo npm install angular-animate
 sudo npm start &
 popd
