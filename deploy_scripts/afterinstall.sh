8fdfbf0ef50ee1eba225d4890c468a4b67b8cc39
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-25 19:36:11 -0400

    Rev

--- deploy_scripts/afterinstall.sh
+++ deploy_scripts/afterinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd .
 cd /var/ibagit/ADS1/
-#need. the pushd and popd because codedeploy with cd
+#need the pushd and popd because codedeploy with cd
 #will break the wholeagent
 apt-get update
 apt-get -y install nodejs npm
