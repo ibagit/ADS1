4dd72111eac12f7fa52ac705d1513662ba122f97
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-26 11:07:08 -0400

    Rev

--- deploy_scripts/npmpkgs.sh
+++ deploy_scripts/npmpkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd .
 #need the pushd and popd because codedeploy with cd
-#will break the wholeagent
+#will break the wholeagent.
 apt-get update
 apt-get -y install git
 cd /var/ibagit/ADS1
