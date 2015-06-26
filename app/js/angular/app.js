fb4e3de7029bbeca0c2bc1e4a4a464676b4d45e6
jasonbohn <jason.bohn@rockdovesolutions.com>
2015-06-26 11:40:45 -0400

    page titles
    page titles fixed

--- app/js/angular/app.js
+++ app/js/angular/app.js
@@ -13,33 +13,46 @@ rdApp.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl : 'pages/default.htm',
-            controller  : 'mapCtrl'
+            controller  : 'mapCtrl',
+            title: 'Food Safety Alert >> Home'
         })
         .when('/force', {
             templateUrl : 'pages/default.htm',
-            controller  : 'tempCtrl'
+            controller  : 'tempCtrl',
+            title: 'Food Safety Alert >> Home' 
         })        
         .when('/contact', {
             templateUrl : 'pages/contact.htm',
-            controller  : 'tempCtrl'
+            controller  : 'tempCtrl',
+            title: 'Food Safety Alert >> Contact Us'
         })
         .when('/about', {
             templateUrl : 'pages/about.htm',
-            controller  : 'tempCtrl'
+            controller  : 'tempCtrl',
+            title: 'Food Safety Alert >> About'
         })        
         .when('/form/:state/:stateCode', {
             templateUrl : 'pages/form.htm',
-            controller  : 'formCtrl'
+            controller  : 'formCtrl',
+            title: 'Food Safety Alert >> Search'
         })        
         .when('/recalls/', {
             templateUrl : 'pages/recalls.htm',
-            controller  : 'resultsCtrl'
+            controller  : 'resultsCtrl',
+            title: 'Food Safety Alert >> Results'
         })
         .when('/recall/:recallId', {
             templateUrl : 'pages/recall.htm',
-            controller  : 'tempCtrl'
+            controller  : 'tempCtrl',
+            title: 'Food Safety Alert >> Result'
         }).
       	otherwise({
         	redirectTo: '/'
 		});
 });
+
+rdApp.run(['$rootScope', '$route', function($rootScope, $route) {
+    $rootScope.$on('$routeChangeSuccess', function() {
+        document.title = $route.current.title;
+    });
+}]);
