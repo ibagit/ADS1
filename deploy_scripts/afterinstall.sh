40eccd0c7cbfe68d327c188ed82342c59ca34522
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-25 21:46:56 -0400

    Rev

--- deploy_scripts/afterinstall.sh
+++ deploy_scripts/afterinstall.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get -y install nodejs npm
 apt-get -y install nodejs-legacy
 sudo npm config set prefix /usr/local
-sudo npm install bower --allow-root
+sudo npm install -g bower --allow-root
 sudo npm install body-parser
 sudo npm install cookie-parser
 sudo npm install debug
@@ -17,6 +17,7 @@ sudo npm install morgan
 sudo npm install serve-favicon
 sudo npm install request
 sudo npm install ejs
+export PATH=/var/ibagit/ADS1/node_modules/bower/bin:$PATH
 sudo bower install angular
 sudo bower install angular-mocks
 sudo bower install jquery
