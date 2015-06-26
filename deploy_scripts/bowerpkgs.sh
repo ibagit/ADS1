45667ccab90cdc2cc4fb4b9174140d70b8bb9669
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-26 00:10:55 -0400

    Rev

--- deploy_scripts/bowerpkgs.sh
+++ deploy_scripts/bowerpkgs.sh
@@ -4,11 +4,11 @@ pushd .
 #will break the wholeagent
 apt-get update
 export PATH=/var/ibagit/ADS1/node_modules/bower/bin:$PATH
-sudo bower install angular
-sudo bower install angular-mocks
-sudo bower install jquery
-sudo bower install bootstrap
-sudo bower install angular-route
-sudo bower install angular-resource
-sudo bower install angular-animate
+bower install angular
+bower install angular-mocks
+bower install jquery
+bower install bootstrap
+bower install angular-route
+bower install angular-resource
+bower install angular-animate
 popd
