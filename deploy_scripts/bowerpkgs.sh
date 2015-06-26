a6c9f00293902a581bff6a75867d90ede77101cc
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-26 00:08:24 -0400

    Rev

--- deploy_scripts/bowerpkgs.sh
+++ deploy_scripts/bowerpkgs.sh
@@ -3,6 +3,12 @@ pushd .
 #need the pushd and popd because codedeploy with cd
 #will break the wholeagent
 apt-get update
-cd /var/ibagit/ADS1
-apt-get -y install bower
+export PATH=/var/ibagit/ADS1/node_modules/bower/bin:$PATH
+sudo bower install angular
+sudo bower install angular-mocks
+sudo bower install jquery
+sudo bower install bootstrap
+sudo bower install angular-route
+sudo bower install angular-resource
+sudo bower install angular-animate
 popd
