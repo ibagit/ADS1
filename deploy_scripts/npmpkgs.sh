cd7e4048224c5aa26ab37831f5664b3b86ec8119
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-25 23:54:36 -0400

    Rev

--- deploy_scripts/npmpkgs.sh
+++ deploy_scripts/npmpkgs.sh
@@ -3,6 +3,7 @@ pushd .
 #need the pushd and popd because codedeploy with cd
 #will break the wholeagent
 apt-get update
+apt-get -y install git
 cd /var/ibagit/ADS1
 apt-get -y install nodejs npm
 apt-get -y install nodejs-legacy
