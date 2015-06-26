1681ca6827c261df40b96c3a2fcc7e537d7b4963
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-26 02:25:02 -0400

    Rev

--- deploy_scripts/npmstart.sh
+++ deploy_scripts/npmstart.sh
@@ -3,5 +3,6 @@ pushd .
 #need the pushd and popd because codedeploy with cd
 #will break the wholeagent
 cd /var/ibagit/ADS1
-sudo nohup bash -c "npm start 2>&1 &"
+sudo nohup bash -c npm start 2>&1 &
 popd
+exit
