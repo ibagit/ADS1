0761ffb828105665cc2f04a061c10804a392af2e
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-29 13:21:02 -0400

    update install syntax

--- deploy_scripts/bowerpkgs.sh
+++ deploy_scripts/bowerpkgs.sh
@@ -5,6 +5,5 @@ pushd .
 apt-get update
 export PATH=/var/ibagit/ADS1/node_modules/bower/bin:$PATH
 cd /var/ibagit/ADS1
-sudo bower install --allow-root
-# angular#1.3 --allow-root
+sudo bower install angular#1.3 --allow-root
 popd
