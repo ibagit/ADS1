7cffcb8806b6e1ff30f6e0ab7e99db8ae4f36a8c
Cleveland Cooke <clevelandcooke@yahoo.com>
2015-06-29 20:14:39 -0400

    updated bower script

--- deploy_scripts/bowerpkgs.sh
+++ deploy_scripts/bowerpkgs.sh
@@ -4,6 +4,10 @@ pushd .
 #will break the wholeagent
 apt-get update
 export PATH=/var/ibagit/ADS1/node_modules/bower/bin:$PATH
+cd /var/ibagit
+sudo chmod 777 -R ADS1
 cd /var/ibagit/ADS1
 bower install angular#1.3.x
+cd /var/ibagit
+sudo chmod 755 -R ADS1
 popd
