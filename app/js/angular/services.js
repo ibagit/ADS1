367e9447f9e419cbc4d53f21a73f636c4a3e0360
Devon-Peroutky <devonperoutky@vividcortex.com>
2015-06-30 14:51:37 -0400

    Broke Safari detection into Service

--- app/js/angular/services.js
+++ app/js/angular/services.js
@@ -26,6 +26,69 @@ rdServices.factory('Validation', function($resource){
     };
 });
 
+// For checking if browser is Safari
+rdServices.factory('Browser', function($resource){
+    return {
+    	isSafari: function() {
+	        console.log("Checking if Safari");
+
+	        // Detect Browser version
+	        var nVer = navigator.appVersion;
+	        var nAgt = navigator.userAgent;
+	        var browserName  = navigator.appName;
+	        var fullVersion  = ''+parseFloat(navigator.appVersion); 
+	        var majorVersion = parseInt(navigator.appVersion,10);
+	        var nameOffset,verOffset,ix;
+
+	        // In Opera 15+, the true version is after "OPR/" 
+	        if ((verOffset=nAgt.indexOf("OPR/"))!=-1) {
+	            browserName = "Opera";
+	            fullVersion = nAgt.substring(verOffset+4);
+	        }
+	        // In older Opera, the true version is after "Opera" or after "Version"
+	        else if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
+				browserName = "Opera";
+				fullVersion = nAgt.substring(verOffset+6);
+ 				if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+					fullVersion = nAgt.substring(verOffset+8);
+	        }
+	        // In MSIE, the true version is after "MSIE" in userAgent
+	        else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
+				browserName = "Microsoft Internet Explorer";
+				fullVersion = nAgt.substring(verOffset+5);
+	        }
+	        // In Chrome, the true version is after "Chrome" 
+	        else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
+				browserName = "Chrome";
+				fullVersion = nAgt.substring(verOffset+7);
+	        }
+	        // In Safari, the true version is after "Safari" or after "Version" 
+	        else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
+				browserName = "Safari";
+				fullVersion = nAgt.substring(verOffset+7);
+				if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+					fullVersion = nAgt.substring(verOffset+8);
+	        }
+	        // In Firefox, the true version is after "Firefox" 
+	        else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
+				browserName = "Firefox";
+				fullVersion = nAgt.substring(verOffset+8);
+	        }
+	        // In most other browsers, "name/version" is at the end of userAgent 
+	        else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < (verOffset=nAgt.lastIndexOf('/')) ) {
+				browserName = nAgt.substring(nameOffset,verOffset);
+				fullVersion = nAgt.substring(verOffset+1);
+				if (browserName.toLowerCase()==browserName.toUpperCase()) {
+          			browserName = navigator.appName;
+				}
+			}
+			browserName = 'Safari';
+	        console.log(browserName);
+	        return browserName === 'Safari';
+	    }
+	};
+});
+
 // State names ---> State Codes
 rdServices.factory('Map', function($resource){
     var map = {
